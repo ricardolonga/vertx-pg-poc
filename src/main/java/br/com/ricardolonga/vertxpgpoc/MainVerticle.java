2c3169c31d0ef3554aae597dbec503119c31fe41
Ricardo Longa <ricardo.longa@neoway.com.br>
2015-02-03 11:23:05 -0200

    Initial commit.

---
+++ src/main/java/br/com/ricardolonga/vertxpgpoc/MainVerticle.java
@@ -0,0 +1,56 @@
+package br.com.ricardolonga.vertxpgpoc;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.MultiMap;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.RouteMatcher;
+import org.vertx.java.core.json.JsonObject;
+import org.vertx.java.platform.Verticle;
+
+public class MainVerticle extends Verticle {
+
+    @Override
+    public void start() {
+        container.deployModule("io.vertx~mod-mongo-persistor~2.1.1-SNAPSHOT", container.config().getObject("mongo-persistor"));
+
+        container.deployWorkerVerticle("br.com.ricardolonga.vertxpgpoc.workers.ZipsWorker", 100);
+
+        RouteMatcher routes = new RouteMatcher().get("/zips", (request) -> {
+            System.out.println(Thread.currentThread().getName() + " - [GET] /zips");
+
+            JsonObject mongoAction = new JsonObject();
+
+            MultiMap params = request.params();
+
+            if (params.contains("fields")) {
+                JsonObject fields = new JsonObject();
+
+                String fields__ = params.get("fields");
+                String[] fields_ = fields__.split(",");
+
+                for (String field : fields_) {
+                    fields.putString(field, "1");
+                }
+
+                mongoAction.putObject("keys", fields);
+            }
+
+            mongoAction.putString("collection", "zips");
+            mongoAction.putString("action", "find");
+            mongoAction.putObject("matcher", new JsonObject());
+
+            vertx.eventBus().send("zips.get", mongoAction, new Handler<Message<JsonObject>>() {
+                @Override
+                public void handle(Message<JsonObject> result) {
+                    System.out.println(Thread.currentThread().getName() + " - " + result.body());
+
+                    request.response().putHeader("Content-Type", "application/json");
+                    request.response().end(result.body().encodePrettily());
+                }
+            });
+        });
+
+        vertx.createHttpServer().requestHandler(routes).listen(8888);
+    }
+
+}
